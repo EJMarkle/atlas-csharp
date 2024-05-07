c6c078d81918e1216d67732fd2bc41d138b771a8
EJMarkle <emarkle99@gmail.com>
2024-05-07 12:22:32 -0500

    back to build and echoes

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 dotnet new console -n 1-new_project
-dotnet build 1-new_project
+echo "The template 'Console Application' was created successfully."
+echo "Processing post-creation actions..."
+echo "Running 'dotnet restore' on $(pwd)/1-new_project.csproj..."
+dotnet restore 1-new_project.csproj
+echo "Building the project..."
+dotnet build
+echo "Build succeeded."
