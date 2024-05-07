41d028821c53b7aea00510c70ef6b5a75bad9202
EJMarkle <emarkle99@gmail.com>
2024-05-07 12:15:06 -0500

    Im dingus

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-mkdir 0-new_project
-cd 0-new_project || exit
 dotnet new console -n 0-new_project
 echo "The template 'Console Application' was created successfully."
 echo "Processing post-creation actions..."
