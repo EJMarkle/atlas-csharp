c470575ddceede690451b7863aea749fa261a6f2
EJMarkle <emarkle99@gmail.com>
2024-05-14 14:09:39 -0500

    xml should be enabled

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -8,43 +8,34 @@ namespace MyMath.Tests
         [Test]
         public void Divide_DividesAllElements_ReturnsNewMatrix()
         {
-            // Arrange
             int[,] inputMatrix = { { 10, 20 }, { 30, 40 } };
             int num = 2;
             int[,] expectedMatrix = { { 5, 10 }, { 15, 20 } };
 
-            // Act
             int[,] result = Matrix.Divide(inputMatrix, num);
 
-            // Assert
             Assert.AreEqual(expectedMatrix, result);
         }
 
         [Test]
         public void Divide_ByZero_ReturnsNull()
         {
-            // Arrange
             int[,] inputMatrix = { { 10, 20 }, { 30, 40 } };
             int num = 0;
 
-            // Act
             int[,] result = Matrix.Divide(inputMatrix, num);
 
-            // Assert
             Assert.IsNull(result);
         }
 
         [Test]
         public void Divide_NullMatrix_ReturnsNull()
         {
-            // Arrange
             int[,] inputMatrix = null;
             int num = 2;
 
-            // Act
             int[,] result = Matrix.Divide(inputMatrix, num);
 
-            // Assert
             Assert.IsNull(result);
         }
     }
