0b7bf11ddfeff83e972d978acaf3bb02af675e8e
EJMarkle <emarkle99@gmail.com>
2024-05-15 16:44:37 -0500

    empty classes w inheritance

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+using System;
+
+/// <summary>
+/// Animal class
+/// </summary>
+class Animal {
+    // Empty class
+}
+
+/// <summary>
+/// Gog class, inherits from Animal
+/// </summary>
+class Dog : Animal {
+    // Empty class
+}
