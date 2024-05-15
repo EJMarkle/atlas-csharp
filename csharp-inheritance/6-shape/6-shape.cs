fa196806df7d73415aa23c0b23300b9b78263b4e
EJMarkle <emarkle99@gmail.com>
2024-05-15 17:42:28 -0500

    lets try again

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,47 +1,62 @@
 using System;
 
 /// <summary>
-/// Base class representing a shape
+/// Shape class
 /// </summary>
-class Shape {
+class Shape
+{
     /// <summary>
-    /// Calculates the area of the shape (to be overridden by derived classes)
+    /// Area method
     /// </summary>
-    /// <returns>The area of the shape</returns>
-    public virtual int Area() {
+    public virtual int Area()
+    {
         throw new NotImplementedException("Area() is not implemented");
     }
 }
 
 /// <summary>
-/// Rectangle class representing a rectangle shape
+/// Rectangle class
 /// </summary>
-class Rectangle : Shape {
+class Rectangle : Shape
+{
     private int width;
     private int height;
 
     /// <summary>
-    /// Width property of the rectangle
+    /// Width property
     /// </summary>
-    public int Width {
-        get { return width; }
-        set {
-            if (value < 0) {
+    public int Width
+    {
+        get {return this.width;}
+        set 
+        {
+            if (value < 0)
+            {
                 throw new ArgumentException("Width must be greater than or equal to 0");
             }
-            width = value;
+            else
+            {
+                this.width = value;
+            }
         }
     }
 
     /// <summary>
-    /// Height property of the rectangle
+    /// Height propety 
     /// </summary>
-    public int Height {
-        get { return height; }
-        set {
-            if (value < 0) {
+    public int Height
+    {
+        get {return this.height;}
+        set
+        {
+            if (value < 0)
+            {
                 throw new ArgumentException("Height must be greater than or equal to 0");
             }
-            height = value;
+            else
+            {
+                this.height = value;
+            }
         }
     }
+}
