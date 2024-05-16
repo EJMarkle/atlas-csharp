e6bdcd07c935ecc9524fd038946308d941652179
EJMarkle <emarkle99@gmail.com>
2024-05-15 20:28:00 -0500

    trying something else

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,15 +1,27 @@
 using System;
 using System.Reflection;
 
+/// <summary>
+/// Abstract base class representing a base object
+/// </summary>
+abstract class Base
+{
+    /// <summary> Name property of the object </summary>
+    public string name { get; set; }
+
+    /// <summary> Overrides the ToString method to provide a custom string representation of the object </summary>
+    public override string ToString()
+    {
+        return $"{this.name} is a {this.GetType().Name}";
+    }
+}
 
 /// <summary>
 /// Interface for interactive objects
 /// </summary>
 interface IInteractive
 {
-    /// <summary>
-    /// Method for interacting with the object
-    /// </summary>
+    /// <summary> Method for interacting with the object </summary>
     void Interact();
 }
 
@@ -18,14 +30,10 @@ interface IInteractive
 /// </summary>
 interface IBreakable
 {
-    /// <summary>
-    /// Durability property indicating the strength of the object
-    /// </summary>
+    /// <summary> Property indicating the durability/strength of the object </summary>
     int durability { get; set; }
 
-    /// <summary>
-    /// Method for breaking the object
-    /// </summary>
+    /// <summary> Method for breaking the object </summary>
     void Break();
 }
 
@@ -34,14 +42,10 @@ interface IBreakable
 /// </summary>
 interface ICollectable
 {
-    /// <summary>
-    /// Indicates whether the object has been collected
-    /// </summary>
+    /// <summary> Property indicating whether the object has been collected </summary>
     bool isCollected { get; set; }
 
-    /// <summary>
-    /// Method for collecting the object
-    /// </summary>
+    /// <summary> Method for collecting the object </summary>
     void Collect();
 }
 
@@ -50,35 +54,25 @@ interface ICollectable
 /// </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
-    /// <summary>
-    /// Durability property indicating the strength of the object
-    /// </summary>
+    /// <summary> Durability property indicating the strength of the object </summary>
     public int durability { get; set; }
 
-    /// <summary>
-    /// Indicates whether the object has been collected
-    /// </summary>
+    /// <summary> Property indicating whether the object has been collected </summary>
     public bool isCollected { get; set; }
 
-    /// <summary>
-    /// Method for interacting with the object
-    /// </summary>
+    /// <summary> Method for interacting with the object </summary>
     public void Interact()
     {
         // Implementation for Interact method
     }
 
-    /// <summary>
-    /// Method for breaking the object
-    /// </summary>
+    /// <summary> Method for breaking the object </summary>
     public void Break()
     {
         // Implementation for Break method
     }
 
-    /// <summary>
-    /// Method for collecting the object
-    /// </summary>
+    /// <summary> Method for collecting the object </summary>
     public void Collect()
     {
         // Implementation for Collect method
