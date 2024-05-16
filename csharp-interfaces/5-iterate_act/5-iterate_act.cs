0f0059816cdee7adffafef7175520948ddd36556
EJMarkle <emarkle99@gmail.com>
2024-05-15 22:23:56 -0500

    /cry

--- csharp-interfaces/5-iterate_act/5-iterate_act.cs
+++ csharp-interfaces/5-iterate_act/5-iterate_act.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 
 /// <summary>
-/// Abstract base class representing a base object
+/// Abstract base class representing an object
 /// </summary>
 abstract class Base
 {
@@ -17,44 +17,6 @@ abstract class Base
     public override string ToString()
     {
         return $"{this.name} is a {this.GetType().Name}";
-    }
-}
-
-/// <summary>
-/// Class representing a room with various objects
-/// </summary>
-class RoomObjects
-{
-    /// <summary>
-    /// Method to iterate through a list of objects and execute actions based on their interfaces
-    /// </summary>
-    /// <param name="roomObjects">List of objects in the room</param>
-    /// <param name="type">Type of interface to execute actions for</param>
-    public static void IterateAction(List<Base> roomObjects, Type type)
-    {
-        foreach (var obj in roomObjects)
-        {
-            // Check if the object implements the specified interface type
-            if (type.IsAssignableFrom(obj.GetType()))
-            {
-                // Execute actions based on the interface type
-                if (type == typeof(IInteractive))
-                {
-                    // If the object is interactive, call the Interact() method
-                    ((IInteractive)obj).Interact();
-                }
-                else if (type == typeof(IBreakable))
-                {
-                    // If the object is breakable, call the Break() method
-                    ((IBreakable)obj).Break();
-                }
-                else if (type == typeof(ICollectable))
-                {
-                    // If the object is collectable, call the Collect() method
-                    ((ICollectable)obj).Collect();
-                }
-            }
-        }
     }
 }
 
@@ -75,9 +37,30 @@ interface IInteractive
 interface IBreakable
 {
     /// <summary>
-    /// Method for breaking the object
+    /// Represents the object's durability
+    /// </summary>
+    int durability { get; set; }
+
+    /// <summary>
+    /// Method for damaging the object
     /// </summary>
     void Break();
+}
+
+/// <summary>
+/// Interface for collectable objects
+/// </summary>
+interface ICollectable
+{
+    /// <summary>
+    /// Indicates whether the object has been collected
+    /// </summary>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// Method for collecting the object
+    /// </summary>
+    void Collect();
 }
 
 /// <summary>
@@ -85,11 +68,28 @@ interface IBreakable
 /// </summary>
 class Door : Base, IInteractive
 {
-    public Door(string name) : base(name) { }
+    /// <summary>
+    /// Constructor for the Door class
+    /// </summary>
+    public Door()
+    {
+        this.name = "Door";
+    }
 
+    /// <summary>
+    /// Constructor for the Door class with an optional parameter
+    /// </summary>
+    public Door(string Name)
+    {
+        this.name = Name;
+    }
+
+    /// <summary>
+    /// Method for interacting with the door
+    /// </summary>
     public void Interact()
     {
-        Console.WriteLine($"You try to open the {name}. It's locked.");
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
     }
 }
 
@@ -98,28 +98,123 @@ class Door : Base, IInteractive
 /// </summary>
 class Decoration : Base, IInteractive, IBreakable
 {
-    private bool isBroken;
+    public bool isQuestItem;
+    public int durability { get; set; }
 
-    public Decoration(string name) : base(name)
+    /// <summary>
+    /// Constructor for the Decoration class
+    /// </summary>
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
     {
-        this.isBroken = false;
+        if (durability <= 0)
+        {
+            throw new Exception("Durability must be greater than 0");
+        }
+        this.name = name;
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
     }
 
+    /// <summary>
+    /// Method for interacting with the decoration
+    /// </summary>
     public void Interact()
     {
-        Console.WriteLine($"You look at the {name}. Not much to see here.");
+        if (this.durability <= 0)
+        {
+            Console.WriteLine($"The {this.name} has been broken.");
+        }
+        else if (isQuestItem)
+        {
+            Console.WriteLine($"You look at the {this.name}. There's a key inside.");
+        }
+        else if (!isQuestItem)
+        {
+            Console.WriteLine($"You look at the {this.name}. Not much to see here.");
+        }
     }
 
+    /// <summary>
+    /// Method for damaging the decoration
+    /// </summary>
     public void Break()
     {
-        if (!isBroken)
+        this.durability--;
+        if (this.durability > 0)
         {
-            Console.WriteLine($"You smash the {name}. What a mess.");
-            isBroken = true;
+            Console.WriteLine($"You hit the {this.name}. It cracks.");
         }
-        else
+        else if (this.durability == 0)
         {
-            Console.WriteLine($"The {name} is already broken.");
+            Console.WriteLine($"You smash the {this.name}. What a mess.");
+        }
+        else if (this.durability < 0)
+        {
+            Console.WriteLine($"The {this.name} is already broken.");
+        }
+    }
+}
+
+/// <summary>
+/// Represents a key, inheriting from Base and implementing the ICollectable interface
+/// </summary>
+class Key : Base, ICollectable
+{
+    public bool isCollected { get; set; }
+
+    /// <summary>
+    /// Constructor for the Key class
+    /// </summary>
+    public Key(string name = "Key", bool isCollected = false)
+    {
+        this.name = name;
+        this.isCollected = isCollected;
+    }
+
+    /// <summary>
+    /// Method for collecting the key
+    /// </summary>
+    public void Collect()
+    {
+        if (!this.isCollected)
+        {
+            this.isCollected = true;
+            Console.WriteLine($"You pick up the {this.name}.");
+        }
+        else if (this.isCollected)
+        {
+            Console.WriteLine($"You have already picked up the {this.name}.");
+        }
+    }
+}
+
+/// <summary>
+/// Represents a room with various objects
+/// </summary>
+class RoomObjects
+{
+    /// <summary>
+    /// Method to iterate through a list of objects and execute actions based on their interfaces
+    /// </summary>
+    public static void IterateAction(List<Base> roomObjects, Type type)
+    {
+        foreach (var obj in roomObjects)
+        {
+            if (type.IsAssignableFrom(obj.GetType()))
+            {
+                if (obj is IInteractive interactiveObj && type == typeof(IInteractive))
+                {
+                    interactiveObj.Interact();
+                }
+                else if (obj is IBreakable breakableObj && type == typeof(IBreakable))
+                {
+                    breakableObj.Break();
+                }
+                else if (obj is ICollectable collectableObj)
+                {
+                    collectableObj.Collect();
+                }
+            }
         }
     }
 }
