6e0a6ee1e3ef6636fe3144268192fa697a04dd5a
EJMarkle <emarkle99@gmail.com>
2024-05-16 18:44:42 -0500

    zom health

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie health, misnomer
+    /// </summary>
+    public class Zombie
+    {
+
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
