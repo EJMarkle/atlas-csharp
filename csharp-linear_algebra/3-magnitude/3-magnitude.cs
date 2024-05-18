42cb8abd590b6d94ad749be1cd37f3ccac9de05d
EJMarkle <emarkle99@gmail.com>
2024-05-18 16:51:45 -0500

    extra curly brace

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,30 +1,28 @@
 using System;
 
+/// <summary>
+/// Math
+/// </summary>
+public class VectorMath
 {
     /// <summary>
-    /// Math
+    /// Gets magnitude of 2d and 3d vectors
     /// </summary>
-    public class VectorMath
+    public static double Magnitude(double[] vector)
     {
-        /// <summary>
-        /// Gets magnitude of 2d and 3d vectors
-        /// </summary>
-        public static double Magnitude(double[] vector)
+        if (vector.Length == 2)
         {
-            if (vector.Length == 2)
-            {
-                double magnitude = Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
-                return Math.Round(magnitude, 2);
-            }
-            else if (vector.Length == 3)
-            {
-                double magnitude = Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
-                return Math.Round(magnitude, 2);
-            }
-            else
-            {
-                return -1;
-            }
+            double magnitude = Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
+            return Math.Round(magnitude, 2);
+        }
+        else if (vector.Length == 3)
+        {
+            double magnitude = Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
+            return Math.Round(magnitude, 2);
+        }
+        else
+        {
+            return -1;
         }
     }
 }
